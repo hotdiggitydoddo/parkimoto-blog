b3aba7dcacd168e09323102674b06e727f2ef4c6
Ryan Dodd <hotdiggitydoddo@gmail.com>
2013-12-05 18:59:27 +0000

    Add fs-extra

--- api/controllers/PhotoController.js
+++ api/controllers/PhotoController.js
@@ -1,12 +1,13 @@
 module.exports = {
 	upload: function(req, res) {
-		var fs = require('fs');
+		var fs = require('fs-extra');
 
 		var tmp_path = req.files.file.path;
 		var target_path = '.tmp/public/images/photos/' + req.files.file.name;
 
 		fs.rename(tmp_path, target_path, function(err) {
 			if (err) throw err;
+			fs.copy(target_path, 'assets/images/photos/' + req.files.file.name, function(err) { });
 			fs.unlink(tmp_path, function() {
 				if (err) throw err;
 				console.log('File uploaded to: ' + target_path);
